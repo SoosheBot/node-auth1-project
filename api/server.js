ccd75e3ba2b742ad01b877e3788794f91a1ffa9f
sooshe <sooshe@sooshebot.com>
2020-02-25 16:51:32 -0600

    remove apiRouter and move over files for clarity

--- api/server.js
+++ api/server.js
@@ -1,21 +1,40 @@
 const express = require('express');
 // const session = require("express-session");
 // const KnexStore = require("connect-session-knex")(session);
+const router = require('express').Router();
 
 
-const apiRouter = require('./apiRouter');
-const configMiddleware = require('./configMiddleware');
-// const knex = require('../data/dbConfig');
-
-const server = express();
-
-configMiddleware(server);
+const authRouter = require('../auth/authRouter');
+const usersRouter = require('../users/usersRouter');
+const restricted = require('../auth/restrictedMiddleware');
 
 
-server.use('/api', apiRouter);
 
-server.get('/', (req, res) => {
-    res.send('Server is running')
+router.use('/auth', authRouter);
+router.use('/users', restricted, usersRouter);
+
+
+router.get('/', (req, res) => {
+    res.send('API is running');
 })
 
-module.exports = server;
+module.exports = router;
+
+
+
+// const apiRouter = require('./apiRouter');
+// const configMiddleware = require('./configMiddleware');
+// // const knex = require('../data/dbConfig');
+
+// const server = express();
+
+// configMiddleware(server);
+
+
+// server.use('/api', apiRouter);
+
+// server.get('/', (req, res) => {
+//     res.send('Server is running')
+// })
+
+// module.exports = server;
