2f923c173c3454baebd5600ed2746fed209374c3
sooshe <sooshe@sooshebot.com>
2020-02-25 16:50:16 -0600

    add logout to authrouter

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -25,6 +25,10 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.userId = user.id;
+        req.session.username = user.username;
+
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
@@ -33,6 +37,20 @@ router.post("/login", (req, res) => {
     .catch(err => {
       res.status(500).json({ message: err });
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: "could not logout" });
+      } else {
+        res.status(200).json({ you: "logged out successfully" });
+      }
+    });
+  } else {
+    res.status(200).json({ bye: "felicia" });
+  }
 });
 
 module.exports = router;
